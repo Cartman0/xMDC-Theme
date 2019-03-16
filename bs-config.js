3c5a092f8a9b1bbf9eec4460dca379298501c1c6
Cartman0 <nabana012@gmail.com>
2019-03-17 04:58:15 +0900

    add first file.

---
+++ bs-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    files: ['build/material.css'],
+    server: {
+        baseDir: "build"
+    },
+    open: false,
+    socket: {
+        domain: 'localhost:3000'
+    },
+};
